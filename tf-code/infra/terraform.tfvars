11ab7ae73a54374d2ef2239c5203f0a905e1e286
tkaburagi <t.kaburagi@me.com>
2020-11-12 18:08:42 +0900

    first commit

---
+++ tf-code/infra/terraform.tfvars
@@ -0,0 +1,17 @@
+#
+## prefix will be added to resources created
+#
+
+prefix = "cts"
+
+# IP address to allow traffic from
+# recommended to use a /32 (single IP address)
+
+allow_from = "0.0.0.0/0"
+
+#
+# additional options 
+#
+
+region = "us-east-1"
+f5_ami_search_name = "F5 BIGIP-15.1.0.4* PAYG-Good 25Mbps*"
---
+++ tf-code/infra/variables.tf
@@ -0,0 +1,18 @@
+variable "prefix" {
+  description = "prefix for resources created"
+  default     = "hashi-f5-demo"
+}
+variable "region" {
+  description = "AWS region"
+  default     = "us-east-1"
+}
+
+variable "f5_ami_search_name" {
+  description = "BIG-IP AMI name to search for"
+  type        = string
+  default     = "F5 BIGIP-15.1.0.4* PAYG-Good 25Mbps*"
+}
+variable "allow_from" {
+  description = "IP Address/Network to allow traffic from (i.e. 192.0.2.11/32)"
+  type	      = string
+}
